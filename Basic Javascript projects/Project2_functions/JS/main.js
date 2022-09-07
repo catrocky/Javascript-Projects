6b1a00e7178f33097ec2234cf2e2cc12ec4e6b04
catrocky <chrisquattrocchi+1@gmail.com>
2022-09-06 18:40:34 -0700

    add project 2

---
+++ Basic Javascript projects/Project2_functions/JS/main.js
@@ -0,0 +1,10 @@
+function buttonfunction() {  //Create a function
+  var str = "This is the button text!";  //Define var
+  document.getElementById("Button_Text").innerHTML = str;
+}
+
+function myFunction(){
+  var sentence="I am a ";
+  sentence+=" sentence."
+  document.getElementById("Concatenate").innerHTML = sentence;
+}
