189d82005bbcb8736484d63cae69052c178da75f
catrocky <chrisquattrocchi+1@gmail.com>
2022-09-09 11:59:11 -0700

    doesn't work

--- Basic Javascript projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic Javascript projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,1 +1,13 @@
 document.write(typeof 3);
+
+function my_Function() {
+  document.getElementById("Test").innerHTML = 0/0;
+}
+
+function my_Function1() {
+  document.getElementById("Test1").innerHTML = isNaN('This is a string');
+}
+
+function my_Function2() {
+  document.getElementById("Test2").innerHTML = isNaN('007');
+}
