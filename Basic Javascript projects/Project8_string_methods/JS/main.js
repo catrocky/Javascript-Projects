6ee50321db40a2423c3bed8853159ab83fab9a3f
catrocky <chrisquattrocchi+1@gmail.com>
2022-09-16 15:28:15 -0700

    update 8

--- Basic Javascript projects/Project8_string_methods/JS/main.js
+++ Basic Javascript projects/Project8_string_methods/JS/main.js
@@ -14,3 +14,42 @@ function slice_Method(){
   var Section=Sentence.slice(14,31);
   document.getElementById("Slice").innerHTML=Section;
 }
+
+//upper case
+function Upper_case() {
+let text = "Hello!";
+let result = text.toUpperCase();
+document.getElementById("demo").innerHTML = result;
+}
+
+//search method
+function Searching() {
+let text = "Will you be around"
+let position = text.search("around");
+document.getElementById("demo2").innerHTML=position;
+}
+//numbers to string
+function string_Method() {
+  var X = 182;
+  document.getElementById("Numbers_to_string").innerHTML=X.toString();
+}
+
+//precision
+function precision_Method() {
+  var Y = 12938.3012987376112;
+  document.getElementById("Precision").innerHTML=Y.toPrecision(10);
+}
+
+//to fixed method
+function toFixed_Method() {
+  let num = 94.789543;
+  let n = num.toFixed(2);
+  document.getElementById("demo3").innerHTML=n;
+}
+
+//value of
+function value_Method() {
+  let wordz = "I am a lot of some texity text";
+  let result = wordz;
+  document.getElementById("demo4").innerHTML=result;
+}
